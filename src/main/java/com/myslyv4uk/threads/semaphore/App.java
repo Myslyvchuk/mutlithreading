1df2ff46397c61bf1430baf4d22ca84b4cf1da76
Bohdan Myslyvchuk <myslyv4uk@gmail.com>
2021-11-12 10:14:01 +0200

    semaphore

---
+++ src/main/java/com/myslyv4uk/threads/semaphore/App.java
@@ -0,0 +1,17 @@
+package com.myslyv4uk.threads.semaphore;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class App {
+	public static void main(String[] args) throws InterruptedException {
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		for(int i = 0;  i< 200; i++) {
+			executorService.submit(() -> Connection.getInstance().connect());
+		}
+		executorService.shutdown();
+		
+		executorService.awaitTermination(1, TimeUnit.DAYS);
+	}
+}
