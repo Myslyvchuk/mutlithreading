55445ff4b038b54a534b715acdbd8409af822cdc
Bohdan Myslyvchuk <myslyv4uk@gmail.com>
2021-11-12 10:13:39 +0200

    deadlock2

---
+++ src/main/java/com/myslyv4uk/threads/deadlock/ThreadRunner.java
@@ -0,0 +1,34 @@
+package com.myslyv4uk.threads.deadlock;
+
+public class ThreadRunner {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		Runner runner = new Runner();
+		
+		Thread thread1 = new Thread(() -> {
+			try {
+				runner.firstThread();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+		
+		Thread thread2 = new Thread(() -> {
+			try {
+				runner.secondThread();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+		thread1.start();
+		thread2.start();
+		
+		thread1.join();
+		thread2.join();
+		
+		runner.finished();
+		//will be executed in the beginning if join is commented
+		System.out.println("main thread finished");
+	}
+}
